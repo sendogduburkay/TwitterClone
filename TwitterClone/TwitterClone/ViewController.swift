1a79b4cc744de484390f29d0993ac072247760a8
Burkay <sendogduburkay@gmail.com>
2023-03-26 14:34:59 +0300

    project created

---
+++ TwitterClone/TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Muhammed Şendoğdu on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
